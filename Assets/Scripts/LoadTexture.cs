514b1bcb0441dbf1320c8945906583a126578421
Edvard Birkeland <edvardbi@stud.ntnu.no>
2021-03-10 16:01:48 +0100

    added images

---
+++ Assets/Scripts/LoadTexture.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+ 
+public class LoadTexture : MonoBehaviour {
+	Texture2D myTexture;
+ 
+	// Use this for initialization
+	void Start () {
+		// load texture from resource folder
+		myTexture = Resources.Load ("Images/hello-world") as Texture2D;
+ 
+		GameObject rawImage = GameObject.Find ("RawImage");
+		rawImage.GetComponent<RawImage> ().texture = myTexture;
+	}
+}
--- Assets/Scripts/ScreenUpdater.cs
+++ Assets/Scripts/ScreenUpdater.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class ScreenUpdater : MonoBehaviour, CannonStateObserver
 {
     public CannonStateHandler stateHandler;
+    public Text speedText;
 
     public void applyChange(CannonState state){
         // TODO: Fix dette
-        gameObject.Text = "Starthastighet: " + this.initSpeed;
+        speedText.text = "Starthastighet: " + state.speed;
     }
 
     void Start()
     {
-        stateHandler.subscribe(this);
+        this.stateHandler.subscribe(this);
+        this.applyChange(this.stateHandler.getCannonState());
+
+        speedText = GameObject.Find("Display_speed").GetComponent<Text>();
     }
 }
