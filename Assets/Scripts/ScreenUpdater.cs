b60d9e0b9ed4cce5def57f5c8418f46c4185f349
Simen Holmestad <simehol@stud.ntnu.no>
2021-03-10 10:21:59 +0100

    Add screenupdater script

---
+++ Assets/Scripts/ScreenUpdater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScreenUpdater : MonoBehaviour, CannonStateObserver
+{
+    public CannonStateHandler stateHandler;
+
+    public void applyChange(CannonState state){
+        // TODO: Fix dette
+        gameObject.Text = "Starthastighet: " + this.initSpeed;
+    }
+
+    void Start()
+    {
+        stateHandler.subscribe(this);
+    }
+}
