3cf12d3c834e761613547240c33dd75ec7085e15
Simen Holmestad <simehol@stud.ntnu.no>
2021-04-07 14:34:17 +0200

    Add vr camera change script

---
+++ Assets/Scripts/SwitchCameraRigOnClick.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SwitchCameraRigOnClick : MonoBehaviour, IPointerClickHandler, CannonStateObserver
+{
+    public GameObject cameraRig;
+    public GameObject originalParent;
+    public GameObject newParent;
+    public CannonStateHandler stateHandler;
+    public bool hasChanged = false;
+    public bool hasLanded = false;
+    public Vector3 startPosition;
+    public Quaternion startRotation;
+
+     IEnumerator returnToStartPosition()
+     {
+        yield return new WaitForSeconds(2.0f);
+
+        // Code to execute after the delay
+        this.cameraRig.transform.SetParent(this.originalParent.transform, false);
+        this.hasChanged = false;
+     }
+
+    public void applyChange(CannonState state){
+        // Checks if the user is at bullet camera and the bullet has landed
+        if (this.hasChanged && this.hasLanded == false && state.hasLanded == true) {
+            StartCoroutine(returnToStartPosition());
+        }
+        this.hasLanded = state.hasLanded;
+    }
+
+    void Start()
+    {
+        this.startPosition = this.originalParent.transform.position;
+        this.startRotation = this.originalParent.transform.rotation;
+        stateHandler.subscribe(this);
+        this.applyChange(this.stateHandler.getCannonState());
+    }
+
+    void changeCamera()
+    {
+        if (!this.hasChanged && this.hasLanded == false) {
+            this.cameraRig.transform.SetParent(this.newParent.transform, false);
+        } else {
+            this.cameraRig.transform.SetParent(this.originalParent.transform, false);
+        }
+        this.hasChanged = !this.hasChanged;
+    }
+
+    private void OnMouseDown()
+    {
+        this.changeCamera();
+    }
+
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        this.changeCamera();
+    }
+}
